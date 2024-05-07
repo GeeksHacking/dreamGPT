e2039a3b8b4c4d12533fa8ab1ef02cb765b58879
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2024-05-07 23:56:07 +0800

    modified it to work as a package

--- dreamgpt/main.py
+++ dreamgpt/main.py
@@ -1,19 +1,34 @@
-'''
-import argparse
 from dreamgpt.constants import MAX_ITERATIONS
 from dreamgpt.engine.dreamEngine import DreamEngine
 
-def main():
+def main(theme):
+    all_dreams = []
     dreams = []
     engine = DreamEngine()
-    parser = argparse.ArgumentParser(description='dreamGPT')
-    parser.add_argument('-t', '--theme', type=str, help='Specify a theme for your dreams')
-    args = parser.parse_args()
-    themeSeeds = engine.expandTheme(args.theme)
+    themeSeeds = engine.expandTheme(theme)
     maxIterations = MAX_ITERATIONS
     while maxIterations > 0:
+        iterations_dreams = {
+            "seeds": [],
+            "dreams": []
+        }
         newDreams = engine.dream(themeSeeds)
+        iterations_dreams["seeds"] = themeSeeds
         combinedDreams = engine.combine(newDreams + dreams)
-        dreams = engine.pick(combinedDreams + newDreams + dreams)
+        dreams = (engine.pick(combinedDreams + newDreams + dreams))
+        for dream in dreams:
+            my_json_object = {
+                "title": dream.title,
+                "description": dream.description,
+                "noveltyScore": dream.noveltyScore,
+                "marketScore": dream.marketScore,
+                "usefulnessScore": dream.usefulnessScore,
+                "easeOfImplementationScore": dream.easeOfImplementationScore,
+                "impactScore": dream.impactScore,
+            }
+            iterations_dreams['dreams'].append(my_json_object)
+        
+        all_dreams.append(iterations_dreams)
+               
         maxIterations -= 1
-'''
+    return all_dreams
